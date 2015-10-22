e244f6b5da4981ce862d32c5519ec7221fbdd6b7
jasonglisson <jason.glisson@gmail.com>
2015-10-22 11:30:46 -0400

    Changed content function in single.php

--- wp-content/themes/babylifehacks/single.php
+++ wp-content/themes/babylifehacks/single.php
@@ -7,7 +7,7 @@
 					<div id="main" class="large-8 medium-8 columns first" role="main">
 						<h2>Baby Life Hack #<?php echo the_ID(); ?></h2>
 					<?php
-						echo '<div class="main">' . wp_strip_all_tags(get_post_field('post_content')) .'</div><br>';	
+						echo '<div class="main">' . wp_strip_all_tags(get_the_content()) .'</div><br>';	
 						echo do_shortcode('[simple-social-share]');
 						echo '<a class="tweet" href="https://twitter.com/intent/tweet?text=' . substr(strip_tags(get_the_content()),0,75) . '...&hashtags=babylifehacks&url=' . wp_get_shortlink() . '" target="_blank">Tweet</a>';
 						echo '<a class="mail" href="mailto:?subject=' . substr(strip_tags(get_the_content()),0,75) . '...&amp;body=' . substr(strip_tags(get_the_content()),0,75) . '...Read the rest of this tip on BabyLifeHacks.com here: ' . wp_get_shortlink() . '">Mail</a>';

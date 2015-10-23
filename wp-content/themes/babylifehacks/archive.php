1da40e169985b6a331773569095248114ea8fdaf
jasonglisson <jason.glisson@gmail.com>
2015-10-23 00:15:16 -0400

    Added archive page

--- wp-content/themes/babylifehacks/archive.php
+++ wp-content/themes/babylifehacks/archive.php
@@ -12,7 +12,28 @@
 				    	</header>
 				
 				    	<!-- To see additional archive styles, visit the /parts directory -->
-				    	<?php get_template_part( 'parts/loop', 'archive' ); ?>					
+				    	<?php //get_template_part( 'parts/loop', 'archive' ); ?>	
+						<?php while ( have_posts() ) : the_post(); 
+					
+								$category = get_the_category($post->ID);
+									foreach($category as $cat) {
+										echo '<div class="entry-content post ' . $cat->slug . ' row">';	
+											echo '<div class="post-icon large-2 hide-for-medium hide-for-small columns"><i title="' . $cat->name . '"></i><span class="icon-text">' . $cat->name . '</span><div class="hack-num">#' . $post->ID . '</div></div>';
+											echo '<div class="post-text large-10 medium-12 small-12 columns">';	
+												my_excerpt(35);	
+												if (get_field('submitted_by', $post->ID)) {
+													echo '<div class="submitted-wrap"><div class="submitted-by">Submitted By ' . get_field('submitted_by', $post->ID) . '</div></div>';		
+												}												
+												echo do_shortcode('[simple-social-share]');
+												echo '<a class="tweet" href="https://twitter.com/intent/tweet?text=' . substr(strip_tags(get_the_content()),0,75) . '...&hashtags=babylifehacks,parenting,parentingtips&url=' . wp_get_shortlink() . '" target="_blank">Tweet</a>';
+												echo '<a class="mail" href="mailto:?subject=' . substr(strip_tags(get_the_content()),0,75) . '...&amp;body=' . substr(strip_tags(get_the_content()),0,75) . '...Read the rest of this tip on BabyLifeHacks.com here: ' . wp_get_shortlink() . '">Mail</a>';
+												echo '<div class="url"><a href="' . wp_get_shortlink() . '" target="_blank"><span title="' . wp_get_shortlink() . '"></span></a></div>';
+												echo '<div class="comments">Comments: <fb:comments-count href="' . get_permalink($post->ID) . '"></fb:comments-count></div>';	
+											echo '</div>';
+										echo '</div>';															
+								} ?>			
+					
+						<?php endwhile; ?>					    					
 			
     				</div> <!-- end #main -->
     
--- wp-content/themes/babylifehacks/search.php
+++ wp-content/themes/babylifehacks/search.php
@@ -19,7 +19,7 @@
 													echo '<div class="submitted-wrap"><div class="submitted-by">Submitted By ' . get_field('submitted_by', $post->ID) . '</div></div>';		
 												}												
 												echo do_shortcode('[simple-social-share]');
-												echo '<a class="tweet" href="https://twitter.com/intent/tweet?text=' . substr(strip_tags(get_the_content()),0,75) . '...&hashtags=babylifehacks&url=' . wp_get_shortlink() . '" target="_blank">Tweet</a>';
+												echo '<a class="tweet" href="https://twitter.com/intent/tweet?text=' . substr(strip_tags(get_the_content()),0,75) . '...&hashtags=babylifehacks,parenting,parentingtips&url=' . wp_get_shortlink() . '" target="_blank">Tweet</a>';
 												echo '<a class="mail" href="mailto:?subject=' . substr(strip_tags(get_the_content()),0,75) . '...&amp;body=' . substr(strip_tags(get_the_content()),0,75) . '...Read the rest of this tip on BabyLifeHacks.com here: ' . wp_get_shortlink() . '">Mail</a>';
 												echo '<div class="url"><a href="' . wp_get_shortlink() . '" target="_blank"><span title="' . wp_get_shortlink() . '"></span></a></div>';
 												echo '<div class="comments">Comments: <fb:comments-count href="' . get_permalink($post->ID) . '"></fb:comments-count></div>';	
